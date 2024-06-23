ffd627e2c3e04e0ac311e7637914310eff6af6d1
Sourav Das <iamsouravdaswade420@gmail.com>
2024-06-23 19:54:56 +0530

    Initial Commit

---
+++ JPXPreditor/JPXPreditorApp.swift
@@ -0,0 +1,17 @@
+//
+//  JPXPreditorApp.swift
+//  JPXPreditor
+//
+//  Created by sourav das on 23/06/24.
+//
+
+import SwiftUI
+
+@main
+struct JPXPreditorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
